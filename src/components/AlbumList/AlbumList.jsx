6ab3b8a3bc14615508d955b2fa65136f4e9b460d
Sam Dahl <samldahl@gmail.com>
2026-03-10 00:06:14 -0600

    add flexbox to album list

--- src/components/AlbumList/AlbumList.jsx
+++ src/components/AlbumList/AlbumList.jsx
@@ -7,15 +7,17 @@ const AlbumList = ({ user, albums = [] }) => {
       {albums.length === 0 ? (
         <p>No albums yet. Create your first album!</p>
       ) : (
-        albums.map((album) => (
-          <div key={album._id}>
-            <Link to={`/albums/${album._id}`}>
-              <h3>{album.albumName}</h3>
-            </Link>
-            <p>Type: {album.type}</p>
-            <p>Date: {album.date}</p>
-          </div>
-        ))
+        <div className="album-grid">
+          {albums.map((album) => (
+            <div key={album._id} className="album-card">
+              <Link to={`/albums/${album._id}`}>
+                <h3>{album.albumName}</h3>
+              </Link>
+              <p>Type: {album.type}</p>
+              <p>Date: {new Date(album.date).toLocaleDateString()}</p>
+            </div>
+          ))}
+        </div>
       )}
     </main>
   );
